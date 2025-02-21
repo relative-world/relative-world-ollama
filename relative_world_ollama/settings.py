3bb28bf4aeb72e497a7e19dce46d384f4a784148
Tyler Evans <tyler@poorlythoughtout.com>
2025-02-21 15:59:00 -0800

    remove templating

--- relative_world_ollama/settings.py
+++ relative_world_ollama/settings.py
@@ -2,13 +2,10 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class OllamaSettings(BaseSettings):
-    base_url: str = "http://192.168.1.14:11434"
-    default_model: str = "qwen2.5:14b"
+    base_url: str = "http://localhost:11434"
+    default_model: str = "qwen2.5:14b"  # we do what we can
     json_fix_model: str = "qwen2.5:14b"
     model_keep_alive: float = 300.0
-
-    jinja2_template_application: str = "relative_world_ollama"
-    jinja2_template_dir: str = "templates"
 
     model_config = SettingsConfigDict(env_prefix='relative_world_ollama_')
 
